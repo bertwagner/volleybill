e02e19c9331a631b2f86a662395cc65d2070592d
Bert Wagner <bertwagner@bertwagner.com>
2023-01-04 12:10:21 -0500

    gets basic insert/update working

--- app/lambda/summarize_game/main.py
+++ app/lambda/summarize_game/main.py
@@ -3,6 +3,7 @@ from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Attr
 import json
 import datetime
+from decimal import Decimal
 
 def process_new_data(data):
     league = data['NewImage']['League']['N']
@@ -57,7 +58,8 @@ def process_new_data(data):
             }
         
         playerSummaries.append({
-            player: playerData
+            "Player": player,
+            "PlayerData": playerData
             })
             
     return (league,season,insertDate,playerSummaries)
@@ -90,28 +92,22 @@ def lambda_handler(event, context):
         data = record['dynamodb']
 
         league,season,insertDate,playerSummaries = process_new_data(data)
-        print('process_new_data():')
-        print(league,season,insertDate,playerSummaries)
 
-        current_items = [get_current_record(league,season,player) for player,playerData in playerSummaries.items()]
-        print('current_items():')
-        print(current_items)
+        current_items = [get_current_record(league,season,p['Player']) for p in playerSummaries]
 
-        
-        for current_item in current_items:
-            print('current_item:')
-            print(current_item)
+        for i,current_item in enumerate(current_items):
+
             if current_item==None:
-                # insert new record
+                # insert new record from process_new_data()
                 new_item = {
-                            'League_Season': f"{current_item['league']}_{current_item['season']}",
-                            'Player': current_item['Player'],
+                            'League_Season': f"{league}_{season}",
+                            'Player': playerSummaries[i]['Player'],
                             'InsertDate': insertDate,
                             'UpdateDate': updateDate,
-                            'DaysPlayed': current_item['PlayerData']['DaysPlayed'],
-                            'GamesWon': current_item['PlayerData']['GamesWon'],
-                            'GamesLost': current_item['PlayerData']['GamesLost'],
-                            'GameWinPercentage': current_item['PlayerData']['GameWinPercentage']
+                            'DaysPlayed': playerSummaries[i]['PlayerData']['DaysPlayed'],
+                            'GamesWon': playerSummaries[i]['PlayerData']['GamesWon'],
+                            'GamesLost': playerSummaries[i]['PlayerData']['GamesLost'],
+                            'GameWinPercentage': Decimal(playerSummaries[i]['PlayerData']['GameWinPercentage'])
                         }
                 table.put_item(
                     Item=new_item,
@@ -121,16 +117,17 @@ def lambda_handler(event, context):
                 # update existing record if this new streamed in record is more up to date than what is already in GameSummary
                 if (current_item['InsertDate'] < insertDate):
                     #update game data
-
-                    daysPlayed=current_item['PlayerData']['DaysPlayed']+playerSummaries[current_item['Player']]['DaysPlayed'] # this is wrong, need to + 1 only if current record has a different game date than the last record. Can't use insert/update dates because might be adding a game after the fact.
-                    gamesWon=current_item['PlayerData']['GamesWon']+playerSummaries[current_item['Player']]['GamesWon']
-                    gamesLost=current_item['PlayerData']['GamesLost']+playerSummaries[current_item['Player']]['GamesLost']
-                    gameWinPercentage=gamesWon/(gamesWon+gamesLost)
+                    print('current_item in update:')
+                    print(current_item)
+                    daysPlayed=0 #current_item['PlayerData']['DaysPlayed']+playerSummaries[current_item['Player']]['DaysPlayed'] # this is wrong, need to + 1 only if current record has a different game date than the last record. Can't use insert/update dates because might be adding a game after the fact.
+                    gamesWon=current_item['GamesWon']+playerSummaries[i]['PlayerData']['GamesWon']
+                    gamesLost=current_item['GamesLost']+playerSummaries[i]['PlayerData']['GamesLost']
+                    gameWinPercentage=Decimal(gamesWon/(gamesWon+gamesLost))
 
                     table.put_item(
                     Item={
-                            'League_Season': f"{current_item['league']}_{current_item['season']}",
-                            'Player': current_item['Player'],
+                            'League_Season': f"{league}_{season}",
+                            'Player': playerSummaries[i]['Player'],
                             'InsertDate': insertDate,
                             'UpdateDate': updateDate,
                             'DaysPlayed': daysPlayed,
