ae2cd88491f86010aaa28116fa9b7a727ae4a7e0
Bert Wagner <bertwagner@bertwagner.com>
2023-01-07 15:34:10 -0500

    remove old tables/lambdas

--- app/lambda/insert_game/main.py
+++ app/lambda/insert_game/main.py
@@ -7,40 +7,80 @@ def lambda_handler(event, context):
     gameData=json.loads(event['body'])
 
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Game')
+    table = dynamodb.Table('VolleyBill')
 
     insertDate = datetime.datetime.utcnow().isoformat()
     
-    try:
-        table.put_item(
-        Item={
-                'League_Season_Date': f"{gameData['League']}_{gameData['Season']}_{gameData['Date']}",
-                'Game': gameData['Game'],
-                'League': gameData['League'],
-                'Season': gameData['Season'],
-                'Date': gameData['Date'],
-                'Teams': gameData['Teams'],
-                'Scores': gameData['Scores'],
-                'InsertDate': insertDate
-            },
-        ConditionExpression='attribute_not_exists(League_Season_Date) AND attribute_not_exists(Game)'   
-        )
-    except ClientError  as e:
-        print(e.response['Error']['Message'])
-        return {
-            "statusCode": 500,
-            'headers': { 'Content-Type': 'application/json' },
-            "body": json.dumps(e.response['Error'])
+    # calculate game and match wins/losses stats
+    playerStats = {}
+
+    for player in gameData['Teams'][0]:
+        if gameData['Scores'][0] > gameData['Scores'][1]:
+            gamesWon=1
+            gamesLost=0
+        else:
+            gamesWon=0
+            gamesLost=1
+
+        stats = {
+            'GamesWon': gamesWon,
+            'GamesLost': gamesLost,
         }
-    
-    for team in gameData['Teams']:
-        for player in team:
-            dynamodb.Table('PlayerGame').put_item(
-                Item={
-                    'League_Season_Player': f"{gameData['League']}_{gameData['Season']}_{player}",
-                    'PlayDate': gameData['Date']
-                }
-            )
+        
+        playerStats[player] = stats
+
+    for player in gameData['Teams'][1]:
+        if gameData['Scores'][1] > gameData['Scores'][0]:
+            gamesWon=1
+            gamesLost=0
+        else:
+            gamesWon=0
+            gamesLost=1
+
+        stats = {
+            'GamesWon': gamesWon,
+            'GamesLost': gamesLost,
+        }
+
+        playerStats[player] = stats
+
+
+    with table.batch_writer() as batch:
+        for team in gameData['Teams']:
+            for player in team:
+                batch.put_item(
+                    Item={
+                        'PK': f"player#{player}",
+                        'SK': f"league#{gameData['League']}_season#{gameData['Season']}",
+                        'InsertDate': insertDate
+                    }
+                )
+                batch.put_item(
+                    Item={
+                        'PK': f"league#{gameData['League']}_season#{gameData['Season']}_player#{player}",
+                        'SK': f"game#{insertDate}",
+                        'InsertDate': insertDate
+                    }
+                )
+                batch.update_item(
+                    Key={
+                        'PK': {"S":f"league#{gameData['League']}_season#{gameData['Season']}_player#{player}"},
+                        'SK': {"S":f"stats"}
+                    },
+                    UpdateExpression='SET GamesWon = GamesWon + :gw, GamesLost = GamesLost + :gl, TotalGamesPlayed = TotalGamesPlayed + 1, UpdateDate = :ud',
+                    ExpressionAttributeValues={
+                        ":gw":{ "N":int(player['GamesWon']) },
+                        ":gl":{ "N":int(player['GamesLost']) },
+                        ":ud":{ "S":insertDate }
+                    }
+                )
+
+        batch.put_item(
+            Item={
+                'PK': f"league#{gameData['League']}_season#{gameData['Season']}",
+                'SK': f"game#{insertDate}"
+            }
+        )
 
     return {
         "statusCode": 200,
