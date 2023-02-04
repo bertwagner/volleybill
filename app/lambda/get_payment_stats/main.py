8af213d6e0252e4a5c8d51742226cc586d1c902d
Bert Wagner <bertwagner@bertwagner.com>
2023-02-04 07:42:25 -0500

    merges payments and dates

--- app/lambda/get_payment_stats/main.py
+++ app/lambda/get_payment_stats/main.py
@@ -27,10 +27,25 @@ def lambda_handler(event, context):
         }
     )
 
-    response = {
-        'payments': payments_response['Items'],
-        'dates': dates_response['Items']
-    }
+    # Combine the data responses
+    from collections import defaultdict
+
+    # Using defaultdict
+    temp = defaultdict(list) 
+    
+    # Using extend
+    for elem in payments_response['Items']:
+        temp[elem['Payer']] = elem
+    
+    for elem in dates_response['Items']:
+        temp[elem['Player']] = elem.update(temp[elem['Player']])
+    
+    #Output = [{"roll_no":y, "school_id":x} for x, y in temp.items()]
+    response = temp
+    # response = {
+    #     'payments': payments_response['Items'],
+    #     'dates': dates_response['Items']
+    # }
 
     results = json.dumps(response, cls=DecimalEncoder)
 
